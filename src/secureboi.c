b0748de9bcfb16b02deb84b781ce094df5dadfcd
NightLamp <benjaminlongbottom98@gmail.com>
2019-03-17 00:00:39 +0800

    added a checklist and printing of each hash in the checklist

--- src/secureboi.c
+++ src/secureboi.c
@@ -58,15 +58,14 @@ int main(int argc, char ** argv) {
 			else if (argc == 3) {
 				
 				printf("storing to your file...\n");
-				int fd = open("/home/ben/.secureboi", O_RDWR | O_APPEND);
+				int fd = open(filePath, O_WRONLY | O_APPEND);
 				if (fd == -1) {
 					perror("secureboi");
 					exit(EXIT_FAILURE);
 				}
 
-				unsigned char fullHash[SHA_DIGEST_LENGTH + 2];	// +2 for \n and \o
-				fullHash[SHA_DIGEST_LENGTH] = '\n';
-				fullHash[SHA_DIGEST_LENGTH + 1] = '\0';
+				unsigned char fullHash[SHA_DIGEST_LENGTH + 1];	// +1 for \o
+				fullHash[SHA_DIGEST_LENGTH] = '\0';
 
 
 				//need to hash the thing
@@ -80,6 +79,64 @@ int main(int argc, char ** argv) {
 				else {
 					close(fd);
 				}
+			}
+		}
+		else if (strcmp(argv[1], "check") == 0) {
+			//check password hashes using "Have I Been Pwned?" database 
+			
+			//is there a password given?
+				//check for that password
+
+			//check all stored passwords
+
+
+			char ** checklist;		//Null terminated checklist
+			char * arr[2];
+			int i = 0;
+
+			if (argc == 3) {
+				//passwd given. have a checklist of 1
+				checklist = arr;
+				checklist[0] = argv[2];
+				i = 1;	//used to add Null terminator later
+			}
+			else if (argc == 2) {
+				//add all stored passwd to checklist
+
+				//open file to read it
+				int fd = open(filePath, O_RDONLY);
+				if (fd == -1) {
+					perror("secureboi");
+					exit(EXIT_FAILURE);
+				}
+
+				unsigned char hashTemp[SHA_DIGEST_LENGTH];
+				int clBufSize = 10;				
+				checklist = calloc(sizeof(char *), clBufSize);
+
+				while (read(fd, hashTemp, SHA_DIGEST_LENGTH) != 0) {
+					//check if read did read the whole thing
+					
+					//add each one to checklist
+					checklist[i++] = strdup(hashTemp);
+					if (i >= clBufSize) {
+						clBufSize += 10;
+						if (realloc(checklist, clBufSize) == NULL) {
+							perror("secureboi check");
+							exit(EXIT_SUCCESS);
+						} 
+					}
+				}
+				close(fd);
+			}
+			//go through all checklist items and see if they are bad
+			checklist[i] = NULL;	//TODO what do if checklist is already full?
+			for (int a = 0; a < i; a++) {
+				printf("0x");
+				for (int c = 0; c < strlen(checklist[a]); c++) {
+					printf("%2x", checklist[a][c]);
+				}
+				printf("\n");
 			}
 		} 
 	}	
